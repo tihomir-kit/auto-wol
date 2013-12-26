d68a3d3ea4783a66e2834b500d182cfe9e7d24e7
pootzko <pootzko@gmail.com>
2013-12-26 15:10:14 +0100

    Added click event handlers.

--- src/net/cmikavac/autowol/MainActivity.java
+++ src/net/cmikavac/autowol/MainActivity.java
@@ -10,9 +10,14 @@ import net.cmikavac.autowol.adapters.*;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	private List<Device> mDevices = new ArrayList<Device>();
@@ -24,6 +29,8 @@ public class MainActivity extends Activity {
 		
 		mDevices = Preferences.getStoredDevices();
 		populateListView();
+		registerClickCallback();
+		registerLongClickCallback();
 	}
 
 	@Override
@@ -37,5 +44,44 @@ public class MainActivity extends Activity {
 		ArrayAdapter<Device> adapter = new DeviceListAdapter(this, mDevices);
 		ListView list = (ListView) findViewById(R.id.device_list_view);
 		list.setAdapter(adapter);		
+	}
+	
+	private void registerClickCallback() {
+		ListView list = (ListView)findViewById(R.id.device_list_view);
+		list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View viewClicked, int position, long id) {
+				
+				Device clickedDevice = mDevices.get(position);
+				String message = "You clicked position " + position
+						+ " which is device " + clickedDevice.getName();
+				Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+			}						
+		});		
+	}	
+	
+	private void registerLongClickCallback() {
+		ListView list = (ListView)findViewById(R.id.device_list_view);
+		list.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+			@Override
+			public boolean onItemLongClick(AdapterView<?> parent, View viewClicked, int position, long id) {
+				showDialog();
+				return false;
+			}
+		});
+	}	
+	
+	private void showDialog() {
+		final CharSequence[] items = {"Wake", "Edit", "Delete"};
+
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setTitle("Choose action");
+		builder.setItems(items, new DialogInterface.OnClickListener() {
+		    public void onClick(DialogInterface dialog, int item) {
+		        Toast.makeText(getApplicationContext(), items[item], Toast.LENGTH_SHORT).show();
+		    }
+		});
+		AlertDialog alert = builder.create();
+		alert.show();
 	}
 }
