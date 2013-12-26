2ad3e7920ddbbfacde1a9699d57c0da1fe8836a8
pootzko <pootzko@gmail.com>
2013-12-26 13:04:57 +0100

    new branch

--- src/net/cmikavac/autowol/adapters/DeviceListAdapter.java
+++ src/net/cmikavac/autowol/adapters/DeviceListAdapter.java
@@ -3,6 +3,7 @@ package net.cmikavac.autowol.adapters;
 import java.util.List;
 
 import net.cmikavac.autowol.R;
+import net.cmikavac.autowol.models.*;
 
 import android.content.Context;
 import android.view.LayoutInflater;
@@ -10,26 +11,26 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
-import net.cmikavac.autowol.models.*;
 
 public class DeviceListAdapter extends ArrayAdapter<Device> {
-	LayoutInflater _inflater = null;
-	List<Device> _devices = null;
+	private Context mContext = null;
+	private List<Device> mDevices = null;
 	
 	public DeviceListAdapter(Context context, List<Device> devices) {
 		super(context, R.layout.device_item_view, devices);
-		_inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		_devices = devices;
+		mContext = context;
+		mDevices = devices;
 	}
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		View itemView = convertView;
-		if (itemView == null) {			
-			itemView = _inflater.inflate(R.layout.device_item_view, parent, false);
+		if (itemView == null) {
+			LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);			
+			itemView = inflater.inflate(R.layout.device_item_view, parent, false);
 		}
 	
-		Device device = _devices.get(position);
+		Device device = mDevices.get(position);
 		
 		TextView nameText = (TextView)itemView.findViewById(R.id.device_item_textName);
 		nameText.setText(device.getName());
